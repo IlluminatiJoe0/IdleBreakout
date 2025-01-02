fcf8d0aef5fe207c4c463dcdf29db0f2dfadcdff
IlluminatiJoe0 <action@dr.com>
2025-01-02 19:00:34 +0100

    Added a "Score" text above score value

--- src/menu.cpp
+++ src/menu.cpp
@@ -15,17 +15,25 @@ Menu::Menu(int x, int y, int width, int height, int xOffset, int yOffset, Game* 
     this->y = y;
     this->width = width;
     this->height = height;
-    this->xOffset = 0;
-    this->yOffset = 0;
+    this->xOffset = xOffset;
+    this->yOffset = yOffset;
     this->game = game;
 }
+
+
 
 void Menu::draw() {
     DrawRectangleLines(this->x, this->y, this->width, this->height, NEON_GREEN);
 
-    std::string buffer = "Score: " + this->game->score.str;
-    int width = MeasureText(buffer.c_str(), 6);
-    DrawText(buffer.c_str(), this->x + this->width / 2 - width / 2, this->y + this->height / 2, 6, NEON_GREEN);
+    // Draw Score value
+    std::string buffer = this->game->score.str;
+    int width = MeasureText(buffer.c_str(), 22);
+    DrawText(buffer.c_str(), this->x + this->width / 2 - width / 2, this->y + this->yOffset * 2, 22, NEON_GREEN);
+
+    // Draw Score text
+    std::string scoreText = "Score";
+    int scWidth = MeasureText(scoreText.c_str(), 18);
+    DrawText(scoreText.c_str(), this->x + this->width / 2 - scWidth / 2, this->y + this->yOffset, 18, NEON_GREEN);
 
 }
 
